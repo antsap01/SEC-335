8e19c23d6610f9e6234eaf55f9f55a3ad945632d
antsap01 <113813026+antsap01@users.noreply.github.com>
2024-02-08 17:01:57 -0500

    Update dns.ps1

--- week3/dns.ps1
+++ week3/dns.ps1
@@ -1,4 +1,4 @@
- param($network, $server) 
+param($network, $server) 
 
 foreach ($ip in 1..254){  
    Resolve-DnsName -DnsOnly "$network,$ip -server $server -ErrorAction Ignore | Select-String -Pattern '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | Where-Object {$_.NameHost -eq '.cyber.local'} 
